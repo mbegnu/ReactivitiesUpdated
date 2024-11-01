e8ff50d4f3bbaf974666dcc64c642cc3fadb97ba
Tano Mbaye <tano.mbaye>
2024-11-01 18:59:26 +0000

    End of section 7

--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -1,13 +1,14 @@
-import React from 'react'
 import { Button, ButtonGroup, Card, CardContent, CardDescription, CardHeader, CardMeta, Image } from 'semantic-ui-react'
-import { Activity } from '../../../app/models/activity'
+import { useStore } from '../../../app/stores/store'
 
-interface Props {
-    activity: Activity;
-    cancelSelectActivity: () => void;
-    openForm: (id: string) => void;
-}
-export default function ActivityDetails({activity, cancelSelectActivity, openForm}: Props) {
+
+export default function ActivityDetails() {
+
+  const {activityStore} = useStore();
+  const {selectedActivity: activity, openForm, cancelSelectedActivity } = activityStore;
+
+  if (!activity) return;
+  
   return (
         <Card fluid>
             <Image src={`/assets/categoryImages/${activity.category}.jpg`}  />
@@ -23,7 +24,7 @@ export default function ActivityDetails({activity, cancelSelectActivity, openFor
             <CardContent extra>
                 <ButtonGroup widths='2'>
                     <Button onClick={() => openForm(activity.id)} basic color='blue' content='Edit'/>
-                    <Button onClick={cancelSelectActivity} basic color='grey' content='Cancel'/>
+                    <Button onClick={cancelSelectedActivity} basic color='grey' content='Cancel'/>
                 </ButtonGroup>
             </CardContent>
     </Card>
--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,17 +1,15 @@
-import React, { ChangeEvent, useState } from 'react'
 import { Button, Form, Segment } from 'semantic-ui-react'
-import { Activity } from '../../../app/models/activity'
+import { useStore } from '../../../app/stores/store';
+import { observer } from 'mobx-react-lite';
+import { ChangeEvent, useState } from 'react';
 
-interface Props {
-    activity: Activity | undefined;
-    closeForm: () => void;
-    createOrEdit: (Activity: Activity) => void;
-    submitting: boolean;
-}
 
-export default function ActivityForm({activity: selectedActivity, closeForm, createOrEdit, submitting}: Props ) {
+export default observer(function ActivityForm() {
 
-    const initialState = selectedActivity ?? {
+    const {activityStore} = useStore();
+    const {closeForm, createActivity, updateActivity, loading} = activityStore;
+
+    const initialState = activityStore.selectedActivity ?? {
         id: '',
         title: '',
         category: '',
@@ -24,7 +22,11 @@ export default function ActivityForm({activity: selectedActivity, closeForm, cre
     const [activity, setActivity] = useState(initialState);
 
     function handleSubmit(){
-        createOrEdit(activity);
+        if (activity.id) {
+            updateActivity(activity);
+        }else{
+            createActivity(activity);
+        }
     }
 
     function handleInputChange(event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>)
@@ -42,9 +44,9 @@ export default function ActivityForm({activity: selectedActivity, closeForm, cre
                 <Form.Input placeholder='Date' type='date' value={activity.date} name='date' onChange={handleInputChange} />
                 <Form.Input placeholder='City' value={activity.city} name='city' onChange={handleInputChange} />
                 <Form.Input placeholder='Venue' value={activity.venue} name='venue' onChange={handleInputChange} />
-                <Button loading={submitting} floated='right' positive type='submit' content='Submit' />
+                <Button loading={loading} floated='right' positive type='submit' content='Submit' />
                 <Button onClick={closeForm} floated='right' type='button' content='Cancel' />
             </Form>
         </Segment>
     )
-}
+})
