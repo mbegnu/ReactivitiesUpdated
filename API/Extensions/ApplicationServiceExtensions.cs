014d328b026d00a12331de53db337ee55cb63476
Tano Mbaye <tano.mbaye>
2024-11-15 16:35:27 +0000

    Part ofModule 16 complete

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -6,6 +6,7 @@ using FluentValidation.AspNetCore;
 using Infrastructure.Security;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
+using Infrastructure.Photos;
 
 namespace API.Extensions
 {
@@ -31,6 +32,8 @@ namespace API.Extensions
             services.AddValidatorsFromAssemblyContaining<Create>();
             services.AddHttpContextAccessor();
             services.AddScoped<IUserAccessor, UserAccessor>();
+            services.AddScoped<IPhotoAccessor, PhotoAccessor>();
+            services.Configure<CloudinarySettings>(config.GetSection("Cloudinary"));
             
             return services;
         }
