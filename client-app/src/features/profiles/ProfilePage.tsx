2f511a27653c13502f8402f9896f8326755593e4
Tano Mbaye <tano.mbaye>
2024-11-16 23:13:16 +0000

    End of section 17

---
+++ client-app/src/features/profiles/ProfilePage.tsx
@@ -0,0 +1,30 @@
+import { Grid } from "semantic-ui-react";
+import ProfileHeader from "./ProfileHeader";
+import ProfileContent from "./ProfileContent";
+import { useParams } from "react-router-dom";
+import { observer } from "mobx-react-lite";
+import { useStore } from "../../app/stores/store";
+import { useEffect } from "react";
+
+export default observer(function ProfilePage() {
+  const {username} = useParams<{username: string}>();
+  const {profileStore} = useStore();
+  const {loadProfile, profile} = profileStore;
+
+  useEffect(() => {
+    if (username) loadProfile(username);
+  }, [loadProfile, username])
+
+  return (
+    <Grid>
+        <Grid.Column width={16}>
+          {profile && 
+            <>
+              <ProfileHeader profile = {profile} />
+              <ProfileContent profile = {profile} />
+            </>
+          }
+        </Grid.Column>
+    </Grid>
+  )
+})
