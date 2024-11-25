6f8b6cbdc4be05a90131648b7f2aa60aadd39c68
Tano Mbaye <tano.mbaye>
2024-11-25 20:37:39 +0000

    Updated CI

--- client-app/src/features/home/HomePage.tsx
+++ client-app/src/features/home/HomePage.tsx
@@ -12,11 +12,11 @@ export default observer(function HomePage() {
         <Container text>
           <Header as='h1' inverted>
             <Image size='massive' src='/assets/logo.png' alt='logo' style={{marginBottom: 12}} />
-            Reactivities
+            Reactivities !!!
           </Header>
           {userStore.isLoggedIn ? (
             <>
-              <Header as='h2' inverted content='Welcome to Reactivities' />
+              <Header as='h2' inverted content='Welcome to Reactivities!' />
               <Button as={Link} to={'/activities'} size='huge' inverted>
                 Go to Activities!
               </Button>
